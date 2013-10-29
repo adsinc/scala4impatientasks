84d5204c2679543bfae031f2f97eb3b260cd9fd9
adsinc <sinc86@gmail.com>
2013-10-29 14:54:35 +0400

    21.5

--- src/org/noip/sinc/chapter21/tasks/task4/FreeApi.scala
+++ src/org/noip/sinc/chapter21/tasks/task4/FreeApi.scala
@@ -15,14 +15,14 @@ import java.util.Scanner
 
 //implicit def askingForToString(af: askingFor) = new Scanner(System.in).next()
 
-object Obtain {
-	def aString(af: askingFor.type)(implicit s: String) = this
-	def anInt(implicit i: Int) = {}
-	def aDouble(implicit d: Double) = {}
-	def askingFor(msg: String) = {}
-}
-
-object askingFor
+//object Obtain {
+//	def aString(af: askingFor.type)(implicit s: String) = this
+//	def anInt(implicit i: Int) = {}
+//	def aDouble(implicit d: Double) = {}
+//	def askingFor(msg: String) = {}
+//}
+//
+//object askingFor
 
 object FreeApi extends App{
 //	def askingFor(msg: String) = new Scanner(System.in).next()
