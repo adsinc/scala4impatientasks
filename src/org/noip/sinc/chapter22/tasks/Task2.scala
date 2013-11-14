bb4e6064f32805de828c7a675abb73f6306632f6
adsinc <sinc86@gmail.com>
2013-11-14 17:35:32 +0400

    tasks

--- src/org/noip/sinc/chapter22/tasks/Task2.scala
+++ src/org/noip/sinc/chapter22/tasks/Task2.scala
@@ -2,21 +2,29 @@ package org.noip.sinc.chapter22.tasks
 
 import scala.io.Source
 import scala.util.continuations._
+import java.io.FileNotFoundException
 
 
 object Task2 extends App{
+
+	def readFile(): String @cpsParam[String, Unit]= {
+		val fileName = shift { k: (String => String) => cont = k ; () }
+		Source.fromFile(fileName, "UTF-8").mkString
+	}
+
 	var cont: (String => String) = null
 	var contents = ""
 
-	reset[String, Unit] {
-		val fileName = shift { k: (String => String) => cont = k ; () }
-		try {
-				contents = Source.fromFile(fileName, "UTF-8").mkString
-		} catch { case _: Throwable => ""}
-		contents
+	try {
+		reset[String, Unit] {
+			contents = readFile()
+			contents
+		}
+	} catch {
+		case _ : FileNotFoundException => contents = ""
 	}
 
-	if(contents == "") {
+	while(contents == "") {
 		println("Try another filename: ")
 		cont(readLine())
 	}
