bf1cd4bba1a9b1af9b107d7da96a989150906c11
Alexey <sinc86@gmail.com>
2014-10-30 09:30:03 +0300

    x

--- src/org/noip/sinc/swing2d/Main.scala
+++ src/org/noip/sinc/swing2d/Main.scala
@@ -1,6 +1,10 @@
 package org.noip.sinc.swing2d
 
+import java.awt.event.{ActionEvent, ActionListener}
+import javax.swing.Timer
+
 import scala.swing._
+import scala.swing.event.Event
 import scala.util.Random
 
 object Main extends App {
@@ -20,9 +24,13 @@ case class Ball(r: Int, x: Int, y: Int) {
 }
 
 class BallsComponent extends Component {
+
+  def apply(e: Event) { println("tick")}
+
   preferredSize = new Dimension(800, 600)
   val rand = new Random
   lazy val balls = createBalls(10)
+  listenTo(new Controller)
 
   def createBalls(n: Int): Seq[Ball] = 0 until n map {_ =>
     new Ball(10, rand.nextInt(size.width), rand.nextInt(size.height))
@@ -31,4 +39,16 @@ class BallsComponent extends Component {
   override protected def paintComponent(g: Graphics2D) = {
     balls foreach (_ draw g)
   }
+}
+
+class Controller extends Publisher {
+  new Timer(100, new ActionListener {
+    def actionPerformed(e: ActionEvent) = {
+      publish(null)
+    }
+  }).start()
+}
+
+object Context extends MainContext {
+  val g: Double = 9.8
 }
