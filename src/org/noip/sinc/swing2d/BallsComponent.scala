15c7922d71ce4daf04d9baac5c6f2e7b91d6f46c
adsinc <sinc86@gmail.com>
2015-01-07 23:05:03 +0300

    base fall

---
+++ src/org/noip/sinc/swing2d/BallsComponent.scala
@@ -0,0 +1,33 @@
+package org.noip.sinc.swing2d
+
+import java.awt.Dimension
+
+import scala.swing._
+
+class BallsComponent extends Component {
+  preferredSize = new Dimension(800, 600)
+  var balls = Seq[Ball]()
+
+  listenTo(new Controller(createBalls(Context.ballCount)))
+
+  reactions += {
+    case be: BallEvent => balls = be.balls; repaint()
+  }
+
+  //todo move to another class
+  def createBalls(n: Int): Seq[Ball] = {
+    if(size.width <= 0 || size.height <= 0) Seq[Ball]()
+    else 0 until n map { _ =>
+//      new Ball(10, Context.random.nextInt(size.width), Context.random.nextInt(size.height))
+      new Ball(10, Context.random.nextInt(size.width), 0)
+    }
+  }
+
+  override protected def paintComponent(g: Graphics2D) = {
+    balls foreach (_ draw g)
+  }
+}
+
+case class Ball(r: Int, x: Int, y: Int, velocity: Double = 0) {
+  def draw(g: Graphics2D) = g.fillOval(x, y, r, r)
+}
--- src/org/noip/sinc/swing2d/Controller.scala
+++ src/org/noip/sinc/swing2d/Controller.scala
@@ -7,11 +7,24 @@ import scala.swing.Publisher
 import scala.swing.event.Event
 
 class Controller(balls: => Seq[Ball]) extends Publisher {
+	var bs: Seq[Ball] = null
 	new Timer(Context.tick, new ActionListener {
 		def actionPerformed(e: ActionEvent) = {
-			publish(BallEvent(balls))
+			if(bs == null || bs.length == 0) bs = balls
+			bs = moveBalls(bs)
+			publish(BallEvent(bs))
 		}
 	}).start()
+
+	def moveBalls(balls: Seq[Ball]) = balls map {
+			b => {
+				val v = b.velocity + dv
+				println(v)
+				b copy (velocity = v, y = (b.y + v).toInt)
+			}
+		}
+
+	val dv = Context.g * Context.tick / 1000
 }
 
 case class BallEvent(balls: Seq[Ball]) extends Event
