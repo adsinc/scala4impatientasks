75207ad85274f9ada3567dfc3caec63d5b3bc429
adsinc <sinc86@gmail.com>
2015-01-14 09:56:05 +0300

    task 20.3

--- src/org/noip/sinc/chapter20/task3/Search.scala
+++ src/org/noip/sinc/chapter20/task3/Search.scala
@@ -1,19 +1,29 @@
 package org.noip.sinc.chapter20.task3
 
+import java.io.File
+
 import scala.actors.Actor
+import scala.util.matching.Regex
 
 object Search extends App {
 
 }
 
-class Visitor extends BaseActor {
+case class Visitor extends BaseActor {
 
-  override def stopActor: Boolean = ???
+  var stopActor = false
 
-  override val onReceive: PartialFunction[Any, Unit] = _
+  val onReceive = {
+    case BaseDirMsg(dir, r) => visitFiles(dir, r)
+    case _ => new Error("Incorrect msg")
+  }
+
+  def visitFiles(f: File, r: Regex): Unit =
+    if(f.isDirectory) f.listFiles() foreach (visitFiles(_, r))
+    else Matcher(r) ! FileMsg(f)
 }
 
-class Matcher extends BaseActor {
+case class Matcher(r: Regex) extends BaseActor {
 
   override def stopActor: Boolean = ???
 
@@ -27,7 +37,9 @@ class Accumulator extends BaseActor {
   override val onReceive: PartialFunction[Any, Unit] = _
 }
 
-case class Msg(msg: String)
+abstract case class Msg(msg: String)
+case class FileMsg(file: File) extends Msg("FileMsg")
+case class BaseDirMsg(dir: File, regex: Regex) extends FileMsg(dir)
 
 trait BaseActor extends Actor {
   def stopActor: Boolean
