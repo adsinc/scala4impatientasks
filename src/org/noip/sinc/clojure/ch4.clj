6136599af9293eaf0efa02fa883aedafb5a10f70
adsinc <sinc86@gmail.com>
2015-09-01 23:02:11 +0300

    clojure

---
+++ src/org/noip/sinc/clojure/ch4.clj
@@ -0,0 +1,12 @@
+(ns org.noip.sinc.clojure.ch4)
+
+(defmacro futures
+  [n & exprs]
+  (vec (for [_ (range n)
+             expr exprs]
+         `(future ~expr))))
+
+(defmacro wait-futures
+  [& args]
+  `(doseq [f# (futures ~@args)]
+     @f#))
