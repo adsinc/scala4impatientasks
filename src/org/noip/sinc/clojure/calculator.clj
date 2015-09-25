f3d0c3ecf5d5936ae390781713a19e97b6ba40db
Aleksey Dolgii <dolgii@devexperts.com>
2015-09-25 13:31:36 +0300

    calculator

--- src/org/noip/sinc/clojure/calculator.clj
+++ src/org/noip/sinc/clojure/calculator.clj
@@ -3,34 +3,40 @@
            (java.awt GridLayout)
            (java.awt.event ActionListener)))
 
-(def operations {
-                 '+ +
-                 '- -
-                 '* *
-                 '/ /
-                 })
+(def conf {
+           :rows 4
+           :cols 3})
 
-(def add-action-listener
-  [component f & args]
-  (.addActionListener component
-                      (proxy [ActionListener] []
-                        (actionPerformed [e]
-                          (apply f e args)))))
+;(def operations {
+;                 '+ +
+;                 '- -
+;                 '* *
+;                 '/ /
+;                 })
+;
+;(def add-action-listener
+;  [component f & args]
+;  (.addActionListener component
+;                      (proxy [ActionListener] []
+;                        (actionPerformed [e]
+;                          (apply f e args)))))
+;
+;(defn create-button
+;  [name op]
+;  (doto (JButton. (str name))
+;    (.addActionListener #())))
+;
+;(map create-button operations)
 
-(defn create-button
-  [name op]
-  (doto (JButton. (str name))
-    (.addActionListener #())))
-
-(map create-button operations)
-
-(defn create-panel [row col]
-  (JPanel. (GridLayout. row col 5 5)))
+(defn create-panel [conf]
+  "Создает главную панель"
+  (let [{rows :rows  cols :cols} conf]
+    (JPanel. (GridLayout. rows cols 5 5))))
 
 (defn create-window []
   "Создает окно калькулятора"
   (doto (JFrame. "Calculator")
-    (.setContentPane (create-panel 4 3))
+    (.setContentPane (create-panel))
     (.pack)
     (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
     (.setVisible true)))
