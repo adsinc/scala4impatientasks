02ac3f6963de493b27ea35e86436a3df51ec55bb
Aleksei Dolgii <sinc86@gmail.com>
2015-11-22 22:41:15 +0300

    logging

--- src/org/noip/sinc/clojure/protocols.clj
+++ src/org/noip/sinc/clojure/protocols.clj
@@ -1,4 +1,6 @@
-(ns org.noip.sinc.clojure.protocols)
+(ns org.noip.sinc.clojure.protocols
+  (:import (clojure.lang IDeref)
+           (org.noip.sinc.clojure.protocols Matrix)))
 
 (defprotocol Matrix
   "Протокол для работы с 2-мерными структурами данных"
@@ -54,3 +56,33 @@
       (if (zero? rs)
         [0 0]
         [rs (count (aget matrix 0))]))))
+
+(deftype SrodingerCat [^:unsynchronized-mutable state]
+  clojure.lang.IDeref
+  (deref [sc]
+    (locking sc
+      (or state
+          (set! state (if (zero? (rand-int 2))
+                        :dead
+                        :alive))))))
+
+(defn shrodinger-cat
+  []
+  (SrodingerCat. nil))
+
+(defrecord Point [x y]
+  Matrix
+  (lookup [_ i j]
+    (when (zero? j)
+      (case i
+        0 x
+        1 y)))
+  (update [pt i j value]
+    (if (zero? j)
+      (condp = i
+        0 (Point. value y)
+        1 (Point. x value))
+      pt))
+  (rows [_] [[x] [y]])
+  (cols [_] [[x y]])
+  (dims [_] [2 1]))
